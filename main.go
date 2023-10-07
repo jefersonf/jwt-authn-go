0b41c23005d74b40795491d72395fb8bb6ab944a
jefersonf <jefersonnpn@gmail.com>
2023-10-07 20:21:13 -0300

    :arrow_up:  add user signup endpoint

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jefersonf/jwt-authn-go/env"
+	"github.com/jefersonf/jwt-authn-go/user"
+)
 
+var (
+	listenAddr string
+)
+
+func init() {
+	env.LoadEnvVars()
+
+	// db connector
+	conn := env.Conn()
+
+	env.SyncDatabase(conn())
+}
+
+func main() {
+	routes := gin.Default()
+	routes.POST("/signup", user.Signup)
+	routes.Run()
 }
