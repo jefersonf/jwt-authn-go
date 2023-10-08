dd553fe0fb79db494343fcde48f14cf34ab66c0b
jefersonf <jefersonnpn@gmail.com>
2023-10-07 22:59:59 -0300

    :bug:  fix exp incorrect type

--- user/service.go
+++ user/service.go
@@ -2,8 +2,11 @@ package user
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/jefersonf/jwt-authn-go/env"
 	"github.com/jefersonf/jwt-authn-go/models"
 	"golang.org/x/crypto/bcrypt"
@@ -26,7 +29,7 @@ func Signup(c *gin.Context) {
 	// hash the password
 	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 	if err != nil {
-		c.JSON(http.StatusBadGateway, gin.H{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "failed to hash password",
 		})
 		return
@@ -53,5 +56,79 @@ func Signup(c *gin.Context) {
 	// response
 	c.JSON(http.StatusOK, gin.H{
 		"message": "signing up completed",
+	})
+}
+
+func Login(c *gin.Context) {
+	// get the email and password off request body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to read body",
+		})
+		return
+	}
+
+	conn := env.Conn()
+	dbHandler := conn()
+
+	// look up requested user
+	var user models.User
+	dbHandler.DB.First(&user, "email=?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid email or password",
+		})
+		return
+	}
+
+	// compare sent in pass with saved user pass hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid password",
+		})
+		return
+	}
+
+	// generate a JWT
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Minute).Unix(),
+	})
+
+	// sign and get complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "failed to create token",
+		})
+		return
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	// sent it
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
+}
+
+func Validate(c *gin.Context) {
+	user, exists := c.Get("user")
+	if !exists {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "user data not found",
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": user,
 	})
 }
